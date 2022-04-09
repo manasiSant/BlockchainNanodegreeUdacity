33aebdc2367c45d015e02ace4e85963ce026c2b7
Manasi Sant <manasi.sant4@gmail.com>
2022-04-09 21:03:20 +0530

    Final commit

--- CapstoneProject/README.md
+++ CapstoneProject/README.md
@@ -12,3 +12,30 @@ The capstone will build upon the knowledge you have gained in the course in orde
 * [Interactive zero knowledge 3-colorability demonstration](http://web.mit.edu/~ezyang/Public/graph/svg.html)
 * [Docker](https://docs.docker.com/install/)
 * [ZoKrates](https://github.com/Zokrates/ZoKrates)
+
+Contract details - 
+
+    Deploying 'SolnSquareVerifier'
+    --------------------
+    > transaction hash:    0x32af8953d24a8cfa7e1656e77bda11fdd26b4ed4f44b843185decc71df7bdcbe
+    > contract address:    0x3DCC6d5a0E80Fe5731fA7e236B83c8D2bB41Ff83
+
+    https://rinkeby.etherscan.io/address/0x3DCC6d5a0E80Fe5731fA7e236B83c8D2bB41Ff83
+
+    Deploying 'Verifier'
+    --------------------
+    > transaction hash:    0xd97ef1b38f4ab71cf55c9c65357f1a935654e62523bad13282451867b7a7f6f0
+    > contract address:    0x2d2C91aFe5db451b6c72be1bC56369c0EC68739E
+
+Minted tokens - transactions are here - 
+    https://rinkeby.etherscan.io/address/0x2d2C91aFe5db451b6c72be1bC56369c0EC68739E
+
+OpenSea details
+    Contract link - https://testnets.opensea.io/assets?search[query]=0x3DCC6d5a0E80Fe5731fA7e236B83c8D2bB41Ff83
+
+Versions - 
+    Truffle v5.5.7 (core: 5.5.7)
+    Ganache v^7.0.3
+    Solidity - 0.5.2 (solc-js)
+    Node v16.13.2
+    Web3.js v1.5.3
--- CapstoneProject/eth-contracts/truffle-config.js
+++ CapstoneProject/eth-contracts/truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ const infuraKey = "v3/e67acc098ae64ab4a3c2c64f56e77bc7";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const fs = require('fs');
+ //const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -60,14 +60,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider('language fit gym amount anchor stomach royal twice mandate infant material used', 
+            `https://rinkeby.infura.io/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000,
+        networkCheckTimeout: 999999
+    },
 
     // Useful for private networks
     // private: {
